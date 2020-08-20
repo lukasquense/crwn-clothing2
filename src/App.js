4ca482a7924d2c6ed14191e7266461a4d079e81f
lukasquense <lukas.quense@gmail.com>
2020-08-20 19:47:28 -0400

    update(collections): update collection preview and item component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/> 
         <Route path='/hats' component={HatsPage}/> 
+        <Route path='/shop' component={ShopPage}/> 
       </Switch>
     </div>
   );
