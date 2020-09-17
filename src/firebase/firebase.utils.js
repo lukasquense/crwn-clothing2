00e1853bec0dcee1956548875aed0b0f8ecd480e
lukasquense <lukas.quense@gmail.com>
2020-09-17 00:53:59 -0300

    feat(firestore): firestore and database connection added

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ const config = {
     measurementId: "G-K5N8YE8T15"
   }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => 
+{
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+  
+  if(!snapShot.exists){
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email, 
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+    console.log('error crearting user', error.message)
+    }
+  }  
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
