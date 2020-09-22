f1391125480300c9987a3ac633130ecee8f6bb4e
lukasquense <lukas.quense@gmail.com>
2020-09-21 22:17:18 -0300

    cart item component added and connected our cart-dropdown with cartItems reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -53,15 +53,28 @@ class  App extends React.Component{
           <Route exact path='/' component={HomePage}/> 
           <Route path='/hats' component={HatsPage}/> 
           <Route path='/shop' component={ShopPage}/> 
-          <Route path='/signin' component={SignInAndSignUpPage}/> 
+          <Route 
+            path='/signin' 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage /> 
+              )
+            } 
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps )(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
