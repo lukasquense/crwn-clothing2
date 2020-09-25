81ff04d54023b5e7d0b1018f61ea4c4b4947b539
lukasquense <lukas.quense@gmail.com>
2020-09-25 00:15:49 -0300

    checkout-items functionalities added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -16,7 +16,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
         {
             cartItems.length ? (
             cartItems.map(cartItem => (
-                <CartItem key={CartItem.id} item={cartItem}/>
+                <CartItem key={cartItem.id} item={cartItem}/>
             ))
             )
             : (
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
+import { removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,6 +18,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                    ) // filter retorna todo lo que de true y saca o filtra lo que da false. Saca el unico que es igual, ya que eso retorna false.
             };
         default:
             return state;
